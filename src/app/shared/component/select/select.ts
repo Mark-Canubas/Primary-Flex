0637af7d018140f6f8e1328ca03114f499f3a454
Mark Canubas <canubasmarkroland@gmail.com>
2025-11-30 20:37:04 +0800

    feat: enhance Select component with detailed documentation and fix overflow issue

--- src/app/shared/component/select/select.ts
+++ src/app/shared/component/select/select.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { SelectModule } from 'primeng/select';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-select',
-  imports: [],
+  imports: [SelectModule, TagModule],
   templateUrl: './select.html',
-  styleUrl: './select.css',
 })
 export class Select {
+  countries = [
+    { name: 'First Sample Long Text That Exceeds Width of the Parent Container' },
+    { name: 'Second Sample Long Text That Exceeds Width of the Parent Container' },
+    { name: 'Third Sample Long Text That Exceeds Width of the Parent Container' },
+    { name: 'Fourth Sample Long Text That Exceeds Width of the Parent Container' },
+
+  ];
+
+  readonly code = `[pt]="{
+      pcOverlay: {
+          root: 'w-full!'
+      },
+      optionLabel:'break-words! whitespace-normal! w-full!'
+  }"`;
 
 }
